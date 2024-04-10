4316a7f15210e2356d5b80b77703b568af071221
LNWatta <helene.finot@le-campus-numerique.fr>
2024-04-10 11:44:49 +0200

    CORS authorizations

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const server = require('./server');
 server.start();
 
+
+
 // const express = require("express");
 // const port = process.env.PORT || 3000;
 //
--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const express = require('express');
 function start() {
   const app = express();
 
+  // Middleware pour les en-têtes CORS
+  app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    next(); // Passe au prochain middleware
+  });
+
   app.get('/', (req, res) => {
     console.log('Got it!');
     res.send('Hello World!');
