74c170cbb6abdb2ba8f53ddbca022a622bb96d3d
LNWatta <helene.finot@le-campus-numerique.fr>
2024-04-12 16:43:41 +0200

    feat error 404 to show wrong file

--- server.js
+++ server.js
@@ -33,17 +33,23 @@ function start() {
     });
 
 
-//  Route pour retourner le contenu de {name}
+    //  Route pour retourner le contenu de {name}
     app.get("/api/drive/:name", async (req, res) => {
         const name = req.params.name
-        try {
-            // Récupération de la liste des fichiers à la racine du dossier temporaire de l'OS
-            const response = await show(path.join(tmpDir, subFolderName, name));
+       // Faire apparaitre l'erreur 400 sur http://localhost:3000/api/drive/nomBidonQuiNexistePas
+        // if (!name){
+        //     return res.status(404).send('Mouhahahah');
+        // }else {
 
-            return res.status(200).json(response);
-        } catch (error) {
-            console.error('Erreur lors de la récupération des fichiers à la racine du drive :', error);
-            res.status(500).json({error: 'Une erreur s\'est produite lors de la récupération des fichiers à la racine du drive.'});
+            try {
+                // Récupération de la liste des fichiers à la racine du dossier temporaire de l'OS
+                const response = await show(path.join(tmpDir, subFolderName, name));
+
+                return res.status(200).json(response);
+            } catch (error) {
+                console.error('Erreur lors de la récupération des fichiers à la racine du drive :', error);
+                res.status(500).json({error: 'Une erreur s\'est produite lors de la récupération des fichiers à la racine du drive.'});
+            }
         }
     });
 
