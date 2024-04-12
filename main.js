5653aba58d07000c2090f5d2d99249dfe99064e7
LNWatta <helene.finot@le-campus-numerique.fr>
2024-04-12 16:17:01 +0200

    feat function show files and folders and show folder content

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
 const server = require('./server');
 server.start();
 
-
-
-// const express = require("express");
-// const port = process.env.PORT || 3000;
-//
-// const app= express();
-//
-// app.get("/", (req, res)=>{
-//     res.send("Hello World !")
-// })
-//
-// app.listen(port, ()=>{
-//     console.log("Serveur est en ligne!");
-// })
--- server.js
+++ server.js
@@ -1,25 +1,57 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
+// Chemin du dossier temporaire de l'OS
+const tmpDir = require('os').tmpdir();
+const subFolderName = 'testnode';
+const {show} = require('./lib/functions');
 
 function start() {
-  const app = express();
+    const app = express();
 
-  // Middleware pour les en-têtes CORS
-  app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "*");
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    next(); // Passe au prochain middleware
-  });
+    // Middleware pour les en-têtes CORS
+    app.use(function (req, res, next) {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "*");
+        res.setHeader("Access-Control-Allow-Headers", "*");
+        next(); // Passe au prochain middleware
+    });
 
-  app.get('/', (req, res) => {
-    console.log('Got it!');
-    res.send('Hello World!');
-  });
 
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+    // Route pour récupérer la liste des fichiers et dossiers à la racine du drive
+    app.get("/api/drive", async (req, res) => {
+        try {
+            const response = await show();
+
+            console.log(response);
+            return res.status(200).json(response);
+        } catch
+            (error) {
+            console.error('Erreur lors de la récupération des fichiers à la racine du drive :', error);
+            res.status(500).json({error: 'Une erreur s\'est produite lors de la récupération des fichiers à la racine du drive.'});
+        }
+    });
+
+
+//  Route pour retourner le contenu de {name}
+    app.get("/api/drive/:name", async (req, res) => {
+        const name = req.params.name
+        try {
+            // Récupération de la liste des fichiers à la racine du dossier temporaire de l'OS
+            const response = await show(path.join(tmpDir, subFolderName, name));
+
+            return res.status(200).json(response);
+        } catch (error) {
+            console.error('Erreur lors de la récupération des fichiers à la racine du drive :', error);
+            res.status(500).json({error: 'Une erreur s\'est produite lors de la récupération des fichiers à la racine du drive.'});
+        }
+    });
+
+
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
 }
 
-module.exports = { start };
+module.exports = {start};
